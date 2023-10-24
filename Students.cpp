16dc0041c044f4bf9c8cd354a8d52ee5480e8db6
Klaudia Denert <klaudiadenert00@gmail.com>
2023-10-24 22:48:56 +0200

    Students

---
+++ Students.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Student {
+    string firstName;
+    string lastName;
+    int studentID;
+};
+
+int main() {
+    vector<Student> students = {
+        {"Kasia", "Kowalska", 78797},
+        {"Jacek", "Wroblewski", 67845},
+        {"Bozena", "Trysuk", 79976}
+    };
+
+    return 0;
+}
